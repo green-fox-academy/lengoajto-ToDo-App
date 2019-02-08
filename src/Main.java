96bcb2e061e550aec3efef61c14ed94eeb1cae9a
Robert Bathory <bathory.robi@gmail.com>
2019-02-08 12:42:21 +0100

    TodoApp Print Usage done.

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Command Line Todo application" + "\n" + "=============================" + "\n" );
+        System.out.println("Command line arguments:");
+        System.out.println(" -l List all the tasks");
+        System.out.println(" -a Add a new task");
+        System.out.println(" -r Removes a task");
+        System.out.println(" -c Completes a task");
+
+
+        }
+    }
+
+
+
